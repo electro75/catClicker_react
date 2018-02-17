a362d19c5ee4d060b97e4d86634cc1b2e74e5e04
sahil n <sahil@mutterfly.in>
2018-02-17 09:17:00 +0530

    adds commenting for better readability

--- src/reducers/active-cat.reducer.js
+++ src/reducers/active-cat.reducer.js
@@ -5,8 +5,9 @@ export default function(state = null, action) {
             return action.payload
 
         case 'CAT_CLICK':
-            return  
-
+            return                  //The doubt is here, I am having trouble figuring out what exactly i need to return.
+                                    //The objective here is to manipulate the active cat object and increase its 'clicks'
+                                    //property by one.
         default:
             return state
     }
